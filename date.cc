310039736f30105d4423dd0c0114317e94a09549
Blakeb57 <bb148917@ohio.edu>
2021-01-31 22:10:21 -0500

    files uploaded

--- date.cc
+++ date.cc
@@ -1,1 +1,134 @@
+/***********************************************************************
+	The implementation file for the date class including the 
+	implementation of overloaded insertion and extraction and
+	comparison operators.
+	John Dolan			September 2005
+		OHIO UNIVERSITY SCHOOL OF EECS
+*************************************************************************/
 
+#include "date.h"
+
+using namespace std;
+
+//DEFAULT CONSTRUCTOR   
+Date::Date()
+{
+    day = month = year = 1;
+
+    //PERMISSABLE DAYS FOR EACH MONTH ARE LOADED INTO AN ARRAY
+    daysallowed[0] = 0;
+    daysallowed[1] = 31;
+    daysallowed[2] = 29;
+    daysallowed[3] = 31;
+    daysallowed[4] = 30;
+    daysallowed[5] = 31;
+    daysallowed[6] = 30;
+    daysallowed[7] = 31;
+    daysallowed[8] = 31;
+    daysallowed[9] = 30;
+    daysallowed[10] = 31;
+    daysallowed[11] = 30;
+    daysallowed[12] = 31;
+}
+
+//CONSTRUCTOR THAT TAKES THREE ARGUMENTS
+Date::Date(int d, int m, int y): day(d), month(m), year(y)
+{
+    //PERMISSABLE DAYS FOR EACH MONTH ARE LOADED INTO AN ARRAY
+	 daysallowed[0] = 0;
+	 daysallowed[1] = 31;
+	 daysallowed[2] = 29;
+	 daysallowed[3] = 31;
+	 daysallowed[4] = 30;
+	 daysallowed[5] = 31;
+	 daysallowed[6] = 30;
+	 daysallowed[7] = 31;
+	 daysallowed[8] = 31;
+	 daysallowed[9] = 30;
+	 daysallowed[10] = 31;
+	 daysallowed[11] = 30;
+	 daysallowed[12] = 31;
+}
+
+//OUTPUT OPERATOR, OVERLOADED AS A FRIEND FUNCTION
+ostream& operator << (ostream& outs, Date d)
+{
+    outs << d.month << '/' << d.day << '/' << d.year;
+    return outs;
+}
+
+//INPUT OPERATOR, OVERLOADED AS A FRIEND
+istream& operator >> (istream& ins, Date d)
+{
+    bool flag = false;
+    string junk;
+
+    ins >> d.month;
+
+    //IF AN INVALID MONTH IS DETECTED THROW A bad_month
+    if(d.month < 1 || d.month > 12)
+    {
+        getline(ins, junk); //EAT THE REST OF THE LINE
+        throw(bad_month(d.month);
+    }
+
+    if(ins.eof()) return ins;
+    if(ins.peek() == '/') ins.ignore();
+
+    ins >> d.year;
+    return ins;
+}
+
+//RETURN WHETHER OR NOT d1 IS GREATER THAN d2
+bool operator > (const Date& d1, const Date& d2)
+{
+    if(d1.year != d2.year)
+    {
+        return (d1.year > d2.year);
+    }else if(d1.month != d2.month)
+    {
+        return (d1.month > d2.month);
+    }else
+    {
+        return (d1.day > d2.day);
+    }
+}
+
+//RETURN WHETHER OR NOT D1 IS LESS THAN D2
+bool operator < (const Date& d1, const Date& d2)
+{
+    if(d1.year != d2.year)
+    {
+        return (d1.year < d2.year);
+    }else if(d1.month != d2.month)
+    {
+        return (d1.month < d2.month);
+    }else
+    {
+        return (d1.day < d2.day);
+    }
+}
+
+//RETURN WHETHER OR NOT d1 EQUALS d2
+bool operator == (const Date& d1, const Date& d2)
+{
+    return (d1.year == d2.year && d1.month == d2.month && d1.day == d2.day);
+}
+
+//RETURN WHETHER OR NOT d1 IS NOT EQUAL TO d2
+bool operator != (const Date& d1, const Date& d2)
+{
+    return (!(d1.year == d2.year && d1.month == d2.month && d1.day == d2.day));
+}
+
+//RETURN WHETHER OR NOT d1 IS LESS THAN OR EQUAL TO d2
+bool operator <= (const Date& d1, const Date& d2)
+{
+    return (d1 == d2 || d1 < d2);
+}
+
+//RETURN WHETHER OR NOT d1 IS GREATER THAN OR EQUAL TO d2
+bool operator >= (const Date& d1, const Date& d2)
+{
+    return (d1 == d2 || d1 > d2);
+}
--- date.h
+++ date.h
@@ -1,1 +1,75 @@
+/*******************************************************************
+//   This class, Date, is designed for the storage of dates.
+	It expects to receive all dates in the m/d/y format, and it 
+	outputs them in the same format. There are no restrictions on
+	the year, but if the momth entered falls outside the normal 
+	bounds a bad_month exception is thrown. The input operator also
+	checks the day entered against a list of permissable days in each
+	month, and throws a bad_day exception if the day is outside 
+	acceptable bounds. There is no mechanism for leap years so all
+	Februaries are allowed 29 days. A full set of comparison operators
+	is included with the class.
+		John Dolan			September 2005
+			OHIO UNIVERSITY SCHOOL OF EECS
+******************************************************************/
 
+#include <iostream>
+#include <ctype.h>
+
+#ifndef DATE
+#define DATE
+// AN EXCEPTION IF THE DATE ENTERED IS INVALID FOR THE MONTH
+
+class bad_day
+{
+    public:
+        bad_day(){monthnum = daynum = 0;}
+        bad_day(int m, int d){monthnum = m, daynum = d;}
+        void msg(){std::cerr << monthnum << " had no day " << daynum << std::endl;}
+
+    private:
+        int monthnum, daynum;
+};
+
+//AN EXCEPTIONFOR AN INVALID MONTH NUMBER
+class bad_month
+{
+    public:
+        bad_month(){monthnum = 0;}
+        bad_month(int m){monthnum = m;}
+        void msg(){std::cerr << monthnum << " is an invalid month number. \n";}
+
+    private:
+        int monthnum;
+};
+
+//A SIMPLE CLASS FOR HOLDING DATES
+class Date
+{
+    public:
+        Date();
+        Date(int d, int m, int y);
+
+        //OUTPUTS THE DATE
+        friend std::ostream& operator << (std::ostream& outs, Date d);
+        //INPUTS THE DDATE
+        friend std::istream& operator >> (std::istream& ins, Date& d);
+
+        /**************************************************
+        COMPARSION OPERATORS
+        **************************************************/
+       friend bool operator > (const Date& d1, const Date& d2);
+       friend bool operator < (const Date& d1, const Date& d2);
+       friend bool operator == (const Date& d1, const Date& d2);
+       friend bool operator != (const Date& d1, const Date& d2);
+       friend bool operator <= (const Date& d1, const Date& d2);
+       friend bool operator >= (const Date& d1, const Date& d2);
+
+    private:
+        int day;
+        int month;
+        int year;
+        int daysallowed[13];
+};
+
+#endif
